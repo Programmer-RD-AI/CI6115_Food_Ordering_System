6a0887e401c707442cbab9923d51edc4f1423416
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-12-19 13:03:46 +0530

    other: Created template to follow through the process for easier understanding of high level overview of the project

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from FOS.services.pizza_customization import PizzaCustomizationService
+class Main(object):
+    def authentication(self):
+        pass
 
-data = {
-    "Crusts": "Thin Crust",
-    "Sauces": ["Mayo", "Tomato"],
-    "Toppings": ["Pepperoni", "Sausage", "Mushrooms"],
-    "Cheese": "Mozzarella",
-}
+    def home_page(self):
+        # This would have their most ordered pizza, and such and there is an option to create a pizza
+        # Also show promotions and such, they would need to be applied as required
+        ...
 
-pizza_customization_service = PizzaCustomizationService(user_configuration=data)
-pizza = pizza_customization_service.apply_customizations()
-print(pizza)
+    # Types of Order
+    def order_new_pizza(self):
+        pass
+
+    def order_already_existing_pizza(self):
+        pass
+
+    # Payment
+    def payment(self):
+        # also include the payment and loyalty program.
+        ...
+
+    def real_time_tracking(self): ...
+    def main(self):
+        pass

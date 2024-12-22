f16649d5b0932d9e32e7df06be140581c00149a2
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-12-22 19:19:42 +0530

    chore: Utils Update

--- FOS/utils/json_handler.py
+++ FOS/utils/json_handler.py
@@ -1,4 +1,5 @@
 import json
+from typing import Self
 
 
 class JSON:
@@ -14,3 +15,19 @@ class JSON:
 
     def get_data(self) -> dict:
         return self.__data
+
+    def set_data(self, new_data: dict) -> None:
+        self.__data = new_data
+        self.write_data()
+
+    def add_data(self, key: str, value: any, assign=True, append=False) -> Self:
+        if append:
+            self.__data[key].append(value)
+        else:
+            self.__data[key] = value
+        self.write_data()
+        return self
+
+    def write_data(self) -> None:
+        with open(self.file_path, "w") as file:
+            json.dump(self.__data, file, indent=4)

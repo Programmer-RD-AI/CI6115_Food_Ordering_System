73c64b5ffa60d3e06cc3d08af4a935fcdb193e44
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-12-25 03:21:11 +0530

    refactor: Builder folder moved

--- FOS/patterns/builder/pizza_builder.py
+++ FOS/patterns/builder/pizza_builder.py
@@ -9,25 +9,47 @@ class PizzaBuilder:
         self.pizza = pizza or Pizza()
         self.quantity: int = 1
 
+    def get_crusts(self) -> list[str]:
+        return self.pizza.get_crusts()
+
     def set_crusts(self, crusts: list[str], **kwargs) -> None:
         self.pizza.set_crusts(crusts, **kwargs)
         return self
+
+    def get_sauces(self) -> list[str]:
+        return self.pizza.get_sauces()
 
     def set_sauces(self, sauces: list[str], **kwargs) -> None:
         self.pizza.set_sauces(sauces, **kwargs)
         return self
 
+    def get_toppings(self) -> list[str]:
+        return self.pizza.get_toppings()
+
     def set_toppings(self, toppings: list[str], **kwargs) -> None:
         self.pizza.set_toppings(toppings, **kwargs)
         return self
+
+    def get_cheeses(self) -> list[str]:
+        return self.pizza.get_cheeses()
 
     def set_cheeses(self, cheeses: list[str], **kwargs) -> None:
         self.pizza.set_cheeses(cheeses, **kwargs)
         return self
 
+    def get_quantity(self) -> list[str]:
+        return self.quantity
+
     def set_quantity(self, quantity: int):
         self.quantity = quantity
         return self
 
-    def build(self):
-        return [self.pizza * self.quantity]
+    def get_packaging(self) -> list[str]:
+        return self.pizza.get_packaging()
+
+    def set_packaging(self, packaging: str):
+        self.pizza.set_packaging(packaging)
+        return self
+
+    def build(self, list=False):
+        return [self.pizza * self.quantity] if list else self.pizza

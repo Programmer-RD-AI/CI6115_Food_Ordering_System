b717ecfc8529021914000d22f59c450ef95a9662
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-12-25 03:17:29 +0530

    Update main __init__.py to remove unwatned imports

--- FOS/__init__.py
+++ FOS/__init__.py
@@ -1,9 +1,3 @@
-from .authentication import *
-from .models import *
-from .patterns import *
-from .repositories import *
-from .services import *
-from .utils import *
 from dotenv import load_dotenv
 
 load_dotenv()
